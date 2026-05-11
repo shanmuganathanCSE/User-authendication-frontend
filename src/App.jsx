b222831b9508f0a84ddc0ffc22a5a647f29e17e4
Shanmuganathan <shanmuganathans2005@gmail.com>
2026-05-11 11:43:17 +0530

    fix vercel react routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Register from "./Register";
-import Login from "./Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <Router>
-      <h1>🔐USER AUTHENDICATION APP</h1>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
